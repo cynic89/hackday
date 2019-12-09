2e8823808ea4ef1b0b3cc59fffe07e9be3f12bc7
cynic89 <shyam_s89@yahoo.com>
2019-12-09 15:39:56 -0800

    add randomization

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/cynic89/hackday/hobbybuddy"
 	"github.com/cynic89/hackday/readSpreadsheet"
+	"math/rand"
 )
 
 func main() {
@@ -12,12 +13,52 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error when trying to login: %s", err.Error())
 	}
-	hobbies := readSpreadsheet.ReadSpreadsheet();
-
+	hobbies := readSpreadsheet.ReadSpreadsheet()
+	randomize(hobbies)
+	
 	for hobby, emails := range hobbies {
 		if len(emails) > 1 {
 			hobbybuddy.CreateEvent(hobby, emails, srv)
 		}
 	}
 
+}
+
+func randomize(hobbies map[string][]string) {
+	randUsers := hobbies["Random"]
+	for _, u := range randUsers {
+		randHobby := randHobby(u, hobbies)
+		if randHobby != "" {
+			hobbies[randHobby] = append(hobbies[randHobby], u)
+		}
+	}
+
+	delete(hobbies, "Random")
+}
+
+func randHobby(user string, hobbies map[string][]string) string {
+	possibleTargets := hobbiesNotPartOf(user, hobbies)
+	if len(possibleTargets) > 0 {
+		randNum := rand.Intn(len(possibleTargets))
+		return possibleTargets[randNum]
+	}
+
+	return ""
+}
+
+func hobbiesNotPartOf(user string, hobbies map[string][]string) (possibleTargets []string) {
+	for hobby, attendees := range hobbies {
+		if !contains(user, attendees) {
+			possibleTargets = append(possibleTargets, hobby)
+		}
+	}
+}
+
+func contains(user string, attendees []string) bool {
+	for _, a := range attendees {
+		if a == user {
+			return true
+		}
+	}
+	return false
 }
